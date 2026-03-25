265416bb869bbf05403ca7688adcc90e36b398ce
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-03-25 09:10:04 -0500

    este es mi primer commit

---
+++ src/main/java/com/mycompany/herencia830/Herencia830.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herencia830;
+
+/**
+ *
+ * @author rosam
+ */
+public class Herencia830 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
